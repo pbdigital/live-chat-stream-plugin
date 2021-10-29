a566cebe27cf7cf0acdf5c69eaa77d5a15717f67
Paul Bright <paulb@pbdigital.com.au>
2021-10-29 00:16:25 +0000

    test updater

--- live-chat-stream.php
+++ live-chat-stream.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              https://pbdigital.com.au/
- * @since             1.0.1
+ * @since             1.0.0
  * @package           Live_Chat_Stream
  *
  * @wordpress-plugin
  * Plugin Name:       Live chat Stream
  * Plugin URI:        https://github.com/pbdigital/live-chat-stream-plugin
  * Description:       
- * Version:           1.0.1
+ * Version:           1.0.0
  * Author:            Paul Bright
  * Author URI:        https://pbdigital.com.au/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'LIVE_CHAT_STREAM_VERSION', '1.0.1' );
+define( 'LIVE_CHAT_STREAM_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
